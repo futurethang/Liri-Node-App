1439c18ad38b5eb9edcb56b0cf2e9613c4c11763
Kevin <kphyde@gmail.com>
2018-10-13 16:47:14 -0700

    got spotify keys to work from env file and return API results

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: "746cefba8c824d8799cddba4d7a5b9a9",
-  secret: "fb550a0149384ee8b9ab7c37f4b6901d"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,12 +1,28 @@
 // FILE REQUIRES
-var dotenv = require("dotenv").config();
-// var Spotify = require('node-spotify-api');
-// // KEY REQUIRES
-// var spotify = new Spotify(keys.spotify);
+var dotenv = require('dotenv').config();
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+
+// KEY REQUIRES
+var spotify = new Spotify(keys.spotify);
 
 // PACKAGE REQUIRES
 var inquirer = require("inquirer");
 
+
+ 
+// var spotify = new Spotify({
+//   id: "746cefba8c824d8799cddba4d7a5b9a9",
+//   secret: "fb550a0149384ee8b9ab7c37f4b6901d"
+// });
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  var results = data.tracks.items;
+console.log(results); 
+});
 
 // var nodeQueryMode = process.argv[2];
 // var nodeQueryText = process.argv.split(3).join(" ");
@@ -14,20 +30,22 @@ var inquirer = require("inquirer");
 // TAKE USER INPUTS
 // use Inquirer Package to offer list choices, send message, and then collect a string to search with
 // https://github.com/sameeri/Code-Inquirer/wiki/Asking-questions-away-with-Inquirer!
-function processAnswers(answers){
+function processAnswers(answers) {  
   console.log("And your answers are:", answers);
 }
 
-var nodeQuery = [
+var searchFunction = [
   {
     message: "Choose a search function:",
     type: "list", 
-    name: "nodeQuery",
+    name: "searchFunction",
     choices: ['search concerts','spotify song search','search for movie','search from .txt']
   },
 ]
 
-inquirer.prompt(nodeQuery, processAnswers)
+inquirer.prompt(searchFunction, processAnswers)
+
+
 
 // DEFINE API CALL FUNCTIONS
 
